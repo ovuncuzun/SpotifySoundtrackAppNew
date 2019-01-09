dc3818b8ebca316ec1bf69601b505a0258b4d15d
ovuncuzun <ovuncuzun@gmail.com>
2019-01-09 00:52:27 +0000

    randomize soundtrack images

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -9,10 +9,12 @@ import { SpotifyService } from '../services/spotify.service';
 export class MovieCardComponent implements OnInit {
 
   soundTracks: any[];
+  currentSoundTrack: any;
   soundTrackImage1: string = "";
   soundTrackImage2: string = "";
   soundTrackImage3: string = "";
   soundTrackImage4: string = "";
+  shuffledSoundTrackImages: any[] = ["", "", "", ""]
 
   constructor(private spotifyService: SpotifyService) { }
 
@@ -23,30 +25,76 @@ export class MovieCardComponent implements OnInit {
   getSoundTracks() {
     this.spotifyService.getSoundTracks().subscribe(p => {
       this.soundTracks = p;
+      this.currentSoundTrack = this.soundTracks[0].track
       this.getSoundTrackImages()
+      
     });
   }
 
   getSoundTrackImages() {
     let queueLength = this.soundTracks.length;
-    let random1 = this.getRandomNumber(0,queueLength - 1)
-    console.log(random1)
-    let random2 = this.getRandomNumber(0,queueLength - 1)
-    console.log(random2)
-    let random3 = this.getRandomNumber(0,queueLength - 1)
-    let random4 = this.getRandomNumber(0,queueLength - 1)
-
-
-    this.soundTrackImage1 = this.soundTracks[random1].track.album.images[1].url;
-    this.soundTrackImage2 = this.soundTracks[random2].track.album.images[1].url;
-    this.soundTrackImage3 = this.soundTracks[random3].track.album.images[1].url;
-    this.soundTrackImage4 = this.soundTracks[random4].track.album.images[1].url;
-
-    console.log(this.soundTrackImage1)
+    do {
+      let random1 = this.getRandomNumber(0,queueLength - 1)
+      this.soundTrackImage1 = this.soundTracks[random1].track.album.images[1].url;
+    } while (this.soundTrackImage1 === this.currentSoundTrack.album.images[1].url)
+    
+    do {
+      let random2 = this.getRandomNumber(0,queueLength - 1)
+      this.soundTrackImage2 = this.soundTracks[random2].track.album.images[1].url;
+    } while (this.soundTrackImage2 === this.currentSoundTrack.album.images[1].url || this.soundTrackImage2 === this.soundTrackImage1)
+    
+    do {
+      let random3 = this.getRandomNumber(0,queueLength - 1)
+      this.soundTrackImage3 = this.soundTracks[random3].track.album.images[1].url;
+    } while (this.soundTrackImage3 === this.currentSoundTrack.album.images[1].url || this.soundTrackImage3 === this.soundTrackImage1 || this.soundTrackImage3 === this.soundTrackImage2)
+    
+    this.shuffledSoundTrackImages = this.shuffleSoundTrackImages()
+    
   }
 
   getRandomNumber(min:number, max:number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  shuffleSoundTrackImages() {
+    let soundTrackImagesArray = [
+      this.currentSoundTrack.album.images[1].url,
+      this.soundTrackImage1,
+      this.soundTrackImage2,
+      this.soundTrackImage3
+    ]
+
+    let currentIndex = soundTrackImagesArray.length
+    let temporaryValue, randomIndex
+   
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = soundTrackImagesArray[currentIndex];
+      soundTrackImagesArray[currentIndex] = soundTrackImagesArray[randomIndex];
+      soundTrackImagesArray[randomIndex] = temporaryValue;
+    }
+
+    return soundTrackImagesArray;
+  }
+
+  playCurrentSoundTrack() {
+    let media = new Audio(this.currentSoundTrack.preview_url)
+    console.log(this.currentSoundTrack)
+    console.log(media)
+    return new Promise((resolve, reject) => { 
+      if("loadeddata") {
+        resolve('ok');
+      } else {
+        reject('error');
+      }
+    }).then(x => media.play())
+
+  }
+
+  startGuessing() {
+    this.playCurrentSoundTrack()
   }
 
 }
